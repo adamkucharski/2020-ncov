df0ba1a0103f87edda103963a2f981d607721210
adamkucharski <adam.kucharski@lshtm.ac.uk>
2020-02-06 22:26:42 +1100

    Update Rmd

--- stoch_model/R/outputs_main.R
+++ stoch_model/R/outputs_main.R
@@ -5,7 +5,7 @@
 # - - -
 # Run bootstrap SMC 
 run_fits(rep_plot=200, # number of repeats
-         nn=1e3,#number of particles
+         nn=1e3, #number of particles
          dt=0.2,
          filename="1"
 )
